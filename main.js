57088ea49b69bd59373b03c046e83ecf04328f34
Sabelo Mkhabela <transformmkhabela@gmail.com>
2025-06-23 14:18:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const root = document.getElementById('root');
+const ctx = root.getContext('2d');
+const w = 600;
+const h = 400;
+const playerW = 20;
+const PlayerH = 100;
 
+
+function drawPlayer(x,y,width,height){
+}
+
+function drawNet(){
+}
+
+function drawBall(){
+}
+
+funtion drawText(x,y,text){
+}
