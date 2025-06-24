a53ae24458848777eb289f39c5aa9aa7cab092fd
Sabelo Mkhabela <transformmkhabela@gmail.com>
2025-06-24 23:30:17 +0200

    Update main.js defined drawing functions

--- main.js
+++ main.js
@@ -6,14 +6,25 @@ const playerW = 20;
 const PlayerH = 100;
 
 
-function drawPlayer(x,y,width,height){
+function drawPlayer(x,y,width,height,color){
+  ctx.fillStyle = color;
+  ctx.fillRect(x,y,width,height)
 }
 
-function drawNet(){
+function drawNet(x,y,width,height,color){
+  ctx.fillStyle = color;
+  ctx.fillRect(x,y,width,height);
 }
 
-function drawBall(){
+function drawBall(x,y,radius,color){
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x,y,radius,0,2*Math.PI);
+  ctx.ClosePath();
 }
 
-funtion drawText(x,y,text){
+function drawText(x,y,text,color){
+  ctx.fillStyle = color;
+  ctx.font = "20px Poppins";
+  ctx.fillText(text,x,y);
 }
